6126a8aeb41da47b4448ff1164c4770377881917
Wojciech Thomas <wojciech.thomas@pwr.edu.pl>
2024-01-12 20:14:56 +0100

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public void welcome() {
+        System.out.println("Start welcome()");
+        System.out.println("Hello from welcome() in App class!");
+        System.out.println("End welcome()");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Start main()");
+        App myApp = new App();
+        myApp.welcome();
+        System.out.println("End main()");
+    }
+}
